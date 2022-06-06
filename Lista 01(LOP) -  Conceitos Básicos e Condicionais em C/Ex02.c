3b92a0ee0969238bb1b56653052437c8e54d7439
Leandro Felix <leandro.almeida3029@gmail.com>
2022-06-06 15:39:27 -0300

    up Ex02

---
+++ Lista 01(LOP) -  Conceitos Básicos e Condicionais em C/Ex02.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+    char char1, char2, operador;
+
+    scanf("%c %c %c", &char1, &operador, &char2);
+
+    printf("-------\n");
+    printf("|%c|%c|r|\n", char1, char2);
+
+    if(operador == '&'){
+        printf("+-+-+-+\n");
+        printf("|0|0|0|\n");
+        printf("+-+-+-+\n");
+        printf("|0|1|0|\n");
+        printf("+-+-+-+\n");
+        printf("|1|0|0|\n");
+        printf("+-+-+-+\n");
+        printf("|1|1|1|\n");
+    } 
+    if(operador == '|'){
+        printf("+-+-+-+\n");
+        printf("|0|0|0|\n");
+        printf("+-+-+-+\n");
+        printf("|0|1|1|\n");
+        printf("+-+-+-+\n");
+        printf("|1|0|1|\n");
+        printf("+-+-+-+\n");
+        printf("|1|1|1|\n");
+    } 
+
+if(operador == '^'){
+        printf("+-+-+-+\n");
+        printf("|0|0|0|\n");;
+        printf("+-+-+-+\n");
+        printf("|0|1|1|\n");
+        printf("+-+-+-+\n");
+        printf("|1|0|1|\n");
+        printf("+-+-+-+\n");
+        printf("|1|1|0|\n");
+    } 
+    printf("-------\n");
+    return 0;
+}
