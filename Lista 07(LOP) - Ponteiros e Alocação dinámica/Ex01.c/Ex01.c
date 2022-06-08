e5a1bc54b25b51c81b152bd0af91db2eece64147
Leandro Felix <leandro.almeida3029@gmail.com>
2022-06-08 16:00:15 -0300

    update

--- Lista 07(LOP) - Ponteiros e Alocação dinámica/Ex01.c/Ex01.c
+++ Lista 07(LOP) - Ponteiros e Alocação dinámica/Ex01.c/Ex01.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    char emails[50];
+    char emails[10][50];
     int i, j;
 
-    fgets(emails, 50, stdin);
+    for(j = 0; j < 10 || emails[j] == 'FIM'; j++){
+        fgets(emails, 10, stdin);
+        printf("%s\n", emails[j]);
+    }
 
-    
-
+/*    for(i = 0; i < 50; i++){
+        if(emails[i] == '@'){
+            i++;
+            while(emails[i] != '.'){
+                printf("%c", emails[i]);
+                i++;
+            }
+        }
+    } 
+*/
     return 0;
 }
--- Lista 07(LOP) - Ponteiros e Alocação dinámica/Ex01.c/Ex02.c
+++ Lista 07(LOP) - Ponteiros e Alocação dinámica/Ex01.c/Ex02.c
@@ -2,7 +2,7 @@
 
 int main() 
 {
-    int n, x, y, cont=0;
+    int n, x, y, j, cont=0;
 
     scanf("%d", &n);
 
@@ -12,19 +12,36 @@ int main()
     for(int i = 0; i < n; i++){
         scanf("%lf", &vet[i]);
     }
-
-    for(int j = n-1; j >= 0; j--){
-        for(x = n-2; x >= 0; x--){
+/*
+    for(int j = n; j >= 0; j--){
+        for(x = n-1; x >= 0; x--){
             if(vet[j] != vet[x]){
                 saida[j] == vet[j];
                 cont++;
             }
         }
     }
+*/
+    for(j = 0; j < n; j++){
+        for(x = 0; j < x; x++){
+            if(j == x){
+                    x++;
+                }
+            if(vet[j] != vet[x]){
+                if(j == x){
+                    x++;
+                }
+                saida[j] = vet[j];
+                cont++;
+                printf("entrou\n");
+            }
+        } 
+    } 
     printf("Contador\n: %d", cont);
     printf("[");
+
     for(int y = 0; y < n; y++){
-        printf("%.4lf,", vet[y]);
+        printf("%.4lf,", saida[y]);
     }
     printf("]");
 
