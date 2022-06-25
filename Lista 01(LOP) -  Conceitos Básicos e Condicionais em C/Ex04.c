4cf53b30e8c42ce170eed38e52c7ea7fd8fa5996
Leandro Felix <leandro.almeida3029@gmail.com>
2022-06-25 11:46:00 -0300

    ok

--- Lista 01(LOP) -  Conceitos Básicos e Condicionais em C/Ex04.c
+++ Lista 01(LOP) -  Conceitos Básicos e Condicionais em C/Ex04.c
@@ -13,12 +13,13 @@ int main(){
 
     if(cond1==1 && cond2==1 && cond3==1 && cond4==1)
         printf("Valores Aceitos");
-    else if((!cond1==1 && cond2==1 && cond3==1 && cond4==1))
+    else if(cond1==0 && cond2==0 && cond3==0 && cond4==0)
         printf("Valores Não Aceitos condições violadas: 1 2 3 4");
-    else if(cond1==0)
-        printf("Valores Não Aceitos condições violadas: 1");
     else if(cond1==0 && cond2==0 && cond3==0)
         printf("Valores Não Aceitos condições violadas: 1 2 3");
+    else if(cond1==0)
+        printf("Valores Não Aceitos condições violadas: 1");
+    
     else if(cond3==0 && cond4==0)
         printf("Valores Não Aceitos condições violadas: 3 4");
     
